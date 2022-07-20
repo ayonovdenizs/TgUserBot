7a2e897cfad2eef23b9caa792a5a4b0d8e01499b
AyonovDenizs <77270222+AyonovDenizs@users.noreply.github.com>
2022-07-20 16:51:47 +0600

    Исправление синтаксиса в install.bat

--- install.bat
+++ install.bat
@@ -2,10 +2,11 @@
 echo Create dir and open with Explorer
 mkdir "TgUserBot by ayonovdenizs_"
 cd "TgUserBot by ayonovdenizs_"
-explorer "TgUserBot by ayonovdenizs_"
+explorer TgUserBot by ayonovdenizs_
 echo Starting download python...
 timeout.exe /t 5
 cls
+echo Download: python-3.10.5.exe
 curl -f -# -O https://www.python.org/ftp/python/3.10.5/python-3.10.5.exe
 echo Starting exe file...
 start python-3.10.5.exe
@@ -15,24 +16,23 @@ echo Installed Python?
 echo Yes - press Enter
 echo No - close CMD window and start again.
 pause
-echo "============================"
-echo "Start install libs - Step 1. Update pip"
-title "Start install libs - Step 1. Update pip"
-echo "============================"
+echo "============================
+echo Start install libs - Step 1. Update pip
+title Start install libs - Step 1. Update pip
+echo ============================
 pip install --upgrade pip
-echo "============================"
+echo ============================
 cls
-echo "============================"
-echo "Start install libs - Step 2. Install Pyrogram"
-title "Start install libs - Step 2. Install Pyrogram"
-echo "============================"
+echo ============================
+echo Start install libs - Step 2. Install Pyrogram
+title Start install libs - Step 2. Install Pyrogram
+echo ============================
 pip install pyrogram
-echo "============================"
+echo ============================
 cls
-echo "============================"
-echo "Start install libs - Step 3. Install tgcrypto"
-title "Start install libs - Step 3. Install tgcrypto"
-echo "============================"
+echo ============================
+echo Start install libs - Step 3. Install tgcrypto
+title Start install libs - Step 3. Install tgcrypto
+echo ============================
 pip install tgcrypto
-echo "============================"
-python downloadbot.py
+echo ============================
