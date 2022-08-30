da35090b2ed06a4b665e10670740d5d05a7a9238
AyonovDenizs <aydenlivestream@gmail.com>
2022-08-30 16:48:50 +0600

    Исправление ошибок.

--- install.bat
+++ install.bat
@@ -6,21 +6,11 @@ explorer "TgUserBot by ayonovdenizs_"
 echo Starting download python...
 timeout.exe /t 5
 cls
-echo Download: python-3.10.5.exe
-curl -f -# -O https://www.python.org/ftp/python/3.10.5/python-3.10.5.exe
-echo Starting exe file...
-start python-3.10.5.exe
-pause
-cls
-echo Installed Python?
-echo Yes - press Enter
-echo No - close CMD window and start again.
-pause
 echo ============================
 echo Start install libs - Step 1. Update pip
 title Start install libs - Step 1. Update pip
 echo ============================
-pip install --upgrade pip
+py -m pip install --upgrade pip
 echo ============================
 cls
 echo ============================
