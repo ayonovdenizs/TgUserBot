8d290d8f75b7cf017d6a90c48c2e572072e17881
AyonovDenizs <aydenlivestream@gmail.com>
2022-08-30 16:42:47 +0600

    Обновление графического установщика

--- install (no using start).py
+++ install (no using start).py
@@ -1,23 +1,27 @@
 from tkinter import *
 import os
- 
+
 class Block:
     def __init__(self, master, func):
-        self.ent = Entry(master, width=20)
-        self.but = Button(master, text="Установить библиотеки")
-        self.lab = Label(master, width=20, bg='black', fg='white')
+        self.ent = Entry(master, width=40, fg='black', bg='black')
+        self.but = Button(master, text="ЗАПУСК УСТАНОВКИ!")
+        self.lab = Label(master, width=40, bg='black', fg='black')
         self.but['command'] = getattr(self, func)
         self.ent.pack()
         self.but.pack()
         self.lab.pack()
  
     def install(self):
-        self.lab = os.system('start install.bat')
+        self.lab = os.system('start install.bat & echo Success!')
+    
+    def start(self):
+        self.lab = Label(master, width=40, bg='black', fg='black')
+        self.lab = os.system('python main.py')
         
  
 root = Tk()
- 
-first_block = Block(root, 'install')
+root.title("Установщик: beta")
 
+first_block = Block(root, 'install')
  
 root.mainloop()
